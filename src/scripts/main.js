b15a8eb8cecf9acd5fac71876d6bca192fe54b33
Егор Елкин <elkinegora@gmail.com>
2019-01-17 15:19:45 +0500

    Растянул холст пропорционально макету

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,32 @@
 import 'phaser';
+import { phaserCheck } from './components/phaser-check';
 
-import { phaserCheck } from './phaser-check';
+let game;
 
-const gameConfig = {
-    width: 680,
-    height: 400,
-    scene: phaserCheck
+window.onload = function() {
+    let gameConfig = {
+        width: 1280,
+        height: 905,
+        scene: phaserCheck,
+        backgroundColor: 0xa1a1a1
+    };
+    game = new Phaser.Game(gameConfig);
+    resize();
+    window.addEventListener("resize", resize);
 };
 
-new Phaser.Game(gameConfig);
+function resize() {
+    const canvas = document.querySelector("canvas");
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const windowRatio = windowWidth / windowHeight;
+    const gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
